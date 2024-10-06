40c17f213f938e5aa0bcad09f447e80fcf5eb6d8
回归天空 <sxp20061207@163.com>
2024-10-06 21:33:11 +0800

    70

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -7,6 +7,7 @@
 
 
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
@@ -15,5 +16,5 @@ mod macros {
 }
 
 fn main() {
-    macros::my_macro!();
+    my_macro!();
 }
