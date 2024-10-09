eefcc4a4b976cd1159ce8f12d5fb23a86ad399ba
回归天空 <sxp20061207@163.com>
2024-10-09 16:32:00 +0800

    Update algorithm1.rs

--- exercises/algorithm/algorithm1.rs
+++ exercises/algorithm/algorithm1.rs
@@ -73,15 +73,26 @@ impl<T> LinkedList<T> {
 		let mut list_c = LinkedList::<T>::new();
         let mut node_a = list_a.start;
         let mut node_b = list_b.start;
-        while node_a.is_some() && node_b.is_some() {
-            let val_a = unsafe { node_a.unwrap().as_ref() }.val;
-            let val_b = unsafe { node_b.unwrap().as_ref() }.val;
+        // while node_a.is_some() && node_b.is_some() {
+        //     let val_a = unsafe { node_a.unwrap().as_ref() }.val;
+        //     let val_b = unsafe { node_b.unwrap().as_ref() }.val;
+        //     if val_a < val_b {
+        //         list_c.add(val_a);
+        //         node_a = unsafe { node_a.unwrap().as_ref() }.next;
+        //     } else {
+        //         list_c.add(val_b);
+        //         node_b = unsafe { node_b.unwrap().as_ref() }.next;
+        //     }
+        // }
+        while let (Some(node_a), Some(node_b)) = (node_a, node_b) {
+            let val_a = unsafe { node_a.as_ref() }.val;
+            let val_b = unsafe { node_b.as_ref() }.val;
             if val_a < val_b {
                 list_c.add(val_a);
-                node_a = unsafe { node_a.unwrap().as_ref() }.next;
+                node_a = unsafe { node_a.as_ref() }.next;
             } else {
                 list_c.add(val_b);
-                node_b = unsafe { node_b.unwrap().as_ref() }.next;
+                node_b = unsafe { node_b.as_ref() }.next;
             }
         }
 		Self {
